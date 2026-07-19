2e7169374cd08625addb95c3d04b7cd407d17ef2
bubblegem <toiletfart54and100@protonmail.com>
2026-07-19 02:25:04 +0000

    Update install.php

--- install.php
+++ install.php
@@ -1000,12 +1000,12 @@ $instance_config =
 	}
 	
 	$page['title'] = 'Installation complete';
-	$page['body'] = '<center>Thank you for using somechan. Please remember to report any bugs you discover. <a href="https://github.com/vichan-devel/vichan/wiki/Configuration-Basics">Please read this if you want to learn to modify the config files.</center></a>';
+	$page['body'] = '<center>Thank you for using somechan. Please remember to report any bugs you discover. <a href="https://github.com/vichan-devel/vichan/wiki/Configuration-Basics">Please read this if you want to learn to modify the config files.<br><br>Wiki for somechan will be set up soon.</center></a>';
 	
 	// Admin panel notice
 	$page['body'] .= '<div class="ban"><h2>Next Steps</h2>' .
-					'<p>The next steps to making your imageboard is logging into the admin panel at <strong><a href="/mod.php">/mod.php</a></strong> using the default credentials:</p>' .
-					'<p><strong>Username:</strong> admin</p>' .
+					'<p>The next steps to making your imageboard is logging into the admin panel at <strong><a href="/mod.php">mod.php</a></strong> using the default credentials:</p>' .
+					'<p><strong>Username:</strong> owner</p>' .
 					'<p><strong>Password:</strong> password</p>' .
 					'<p><strong>Important: For security, please change the password of the owner account immediately after logging in, and in order to prevent a <a href="https://soyjakwiki.org/Captain Coal attack">CCA (Captain Coal Attack)</a>.</strong></p></div>';
 					//'<p style="text-align:center"><a href="/mod.php"><button>Go to Admin Panel</button></a></p></div>';

93138153f69d29be443b217d9f260b5ea316f83f
bubblegem <toiletfart54and100@protonmail.com>
2026-07-19 04:32:47 +0000

    Update report.php

--- report.php
+++ report.php
@@ -1,19 +1,38 @@
 <?php
 require 'inc/bootstrap.php';
+
 $global = isset($_GET['global']);
 $post = (isset($_GET['post']) ? $_GET['post'] : false);
-$board = (isset($_GET['board']) ? $_GET['board'] : false);
+$board_uri = (isset($_GET['board']) ? $_GET['board'] : false);
 
-if (!$post || !preg_match('/^delete_\d+$/', $post) || !$board || !openBoard($board)) {
-	header('HTTP/1.1 400 Bad Request');
-	error(_('Bad request.'));
+if (!$post || !preg_match('/^delete_\d+$/', $post) || !$board_uri || !openBoard($board_uri)) {
+    header('HTTP/1.1 400 Bad Request');
+    error(_('Bad request.'));
 }
 
 if ($config['report_captcha']) {
-	$captcha = generate_captcha($config['captcha']['extra']);
+    $captcha = generate_captcha($config['captcha']['extra']);
 } else {
-	$captcha = null;
+    $captcha = null;
 }
 
-$body = Element($config['file_report'], ['global' => $global, 'post' => $post, 'board' => $board, 'captcha' => $captcha, 'config' => $config]);
-echo Element($config['file_page_template'], ['config' => $config, 'body' => $body]);
+$body = Element($config['file_report'], [
+    'global' => $global,
+    'post' => $post,
+    'board' => $board_uri,
+    'captcha' => $captcha,
+    'config' => $config
+]);
+
+global $board;
+
+$title = $board['url'] . ' - ' . $board['title'] . '; Report window';
+$subtitle = $board['subtitle'] ?? '';
+
+echo Element($config['file_page_template'], [
+    'config' => $config,
+    'title' => $title,
+    'subtitle' => $subtitle,
+    'boardlist' => createBoardlist(),
+    'body' => $body
+]);
